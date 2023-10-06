4903522a8bbb8132e13518ded4876ac5fb13e618
Anand Shah <42813783+anandshah98@users.noreply.github.com>
2023-10-06 01:42:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,9 @@ model, tokenizer = qamodel.load_sharded_model()
 def request_answer(query):
     search_results = PI.search(query)
     answers = []
-    # print(search_results)
     for r in search_results['matches']:
         if r['score'] >= 0.45:
             tokenized_context = tokenizer(r['metadata']['text'])
-#             query_to_model = f"""You are doctor who knows cures to diseases. If you don't know the answer, please refrain from providing answers that are not relevant to the context. Please suggest appropriate remedies based on the context provided.\n\nContext: {context}\n\n\nResponse: """
             query_to_model = """You are doctor who knows cures to diseases. If you don't know, say you don't know. Please respond appropriately based on the context provided.\n\nContext: {}\n\n\nResponse: """
             for ind in range(0, len(tokenized_context['input_ids']), 512-42):                        
                 decoded_tokens_for_context = tokenizer.batch_decode([tokenized_context['input_ids'][ind:ind+470]], skip_special_tokens=True)
@@ -40,7 +38,7 @@ demo = gr.Interface(
     ],
     cache_examples=True,
     title="MedQA assistant",
-    #description="MedQA assistant"
+    description='Check out the repository at: https://github.com/anandshah98/MedQA',
 )
 
 demo.launch()
